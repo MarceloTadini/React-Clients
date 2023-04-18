3598f1cc86c8970c0e259ddc385700d9736cb80d
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-04-18 17:06:45 -0300

    React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function App(){
     return(
-        <h1>Ois</h1>
+        <div>
+            oi
+        </div>
     )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
+import ErrorPage from "./pages/ErrorPage";
+import  ReactDOM  from "react-dom";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <ErrorPage/>
+    },
+])
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={router} />
     </React.StrictMode>,
     document.getElementById('root')
 )
