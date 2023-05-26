e317154aabdd7456484cb64ecf2d8093ea6de443
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-05-26 18:12:40 -0300

    Saving forms data

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,52 @@
-import Header from "../partials/Header/Header"
+import {TextField, Button, Typography} from '@mui/material';
+import { useState } from 'react';
 
 function Login(){
+
+    const [form, setForm] = useState({
+        email:'',
+        password:'',
+    })
+
+    function handleInputChange(event){
+        const {name, value} = event.target
+
+        setForm({
+            ...form,
+            [name]: value,
+        })
+    }
+
+    function handleFormSubmit(){
+        console.log(form)
+    } 
+
     return(
         <>  
-            <Header/>
             <h2>Login</h2>
+
+            <Typography variant="h3">Acesso Restrito</Typography>
+
+            <div>
+                <TextField
+                  label="Digite o seu email"
+                  name="email"
+                  onChange={handleInputChange}
+                />
+            </div>
+
+            <div>
+                <TextField
+                  label="Digite sua senha"
+                  name="password"
+                  onChange={handleInputChange}
+                  type='password'
+                />
+            </div>
+
+            <div>
+                <Button onClick={handleFormSubmit} variant="contained" color='primary'>Entrar</Button>
+            </div>
         </>
     )
 }
